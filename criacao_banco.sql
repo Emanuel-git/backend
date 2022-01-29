1b16f6d3e8fe497d6d716ea97789ed6133778846
Emanuel <jose.eosousa@gmail.com>
2022-01-28 22:12:13 -0300

    arquivo do banco (pode sofrer alterações)

---
+++ criacao_banco.sql
@@ -0,0 +1,22 @@
+-- Banco de dados utlizado: Mysql
+
+-- Criação do banco de dados 'backend_genion'
+create database backend_genion;
+
+use backend_genion;
+
+-- Criação da tabela 'categorias'
+create table categorias (
+    codigo varchar(10) primary key not null,
+    nome varchar(50) not null
+);
+
+-- Criação da tabela 'produtos'
+create table produtos (
+    nome varchar(100) not null,
+    sku varchar(10) primary key not null,
+    preco float not null,
+    descricao text,
+    quantidade int not null,
+    codigo_categoria varchar(50) not null
+);
